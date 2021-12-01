d118f52b4339725229237a772171b5581216562e
Rodrigo Zampieri Castilho <rodrigo.zampieri@gmail.com>
2021-12-01 12:40:30 -0300

    Initial commit

---
+++ lib/advent_of_code2021_elixir.ex
@@ -0,0 +1,18 @@
+defmodule AdventOfCode2021Elixir do
+  @moduledoc """
+  Documentation for `AdventOfCode2021Elixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventOfCode2021Elixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/advent_of_code2021_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule AdventOfCode2021ElixirTest do
+  use ExUnit.Case
+  doctest AdventOfCode2021Elixir
+
+  test "greets the world" do
+    assert AdventOfCode2021Elixir.hello() == :world
+  end
+end
